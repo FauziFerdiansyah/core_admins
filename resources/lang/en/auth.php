4d4f34e97cf63cd1c250cc59793227fb4b37850b
teguhmaulana25 <maulanateguh87@gmail.com>
2018-08-26 17:39:14 +0700

    Update lang auth

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,9 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    // 'failed' => 'These credentials do not match our records.',
+    // 'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Maaf username atau kata sandi anda salah. Silahkan coba kembali.',
+    'throttle' => 'Terlalu banyak login. Silahkan coba kembali dalam :seconds detik.',
 
 ];
--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,15 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    // 'password' => 'Passwords must be at least six characters and match the confirmation.',
+    // 'reset' => 'Your password has been reset!',
+    // 'sent' => 'We have e-mailed your password reset link!',
+    // 'token' => 'This password reset token is invalid.',
+    // 'user' => "We can't find a user with that e-mail address.",
+    'password' => 'Kata sandi harus setidaknya enam karakter dan sesuai dengan konfirmasi.',
+    'reset' => 'Kata sandi Anda telah diatur ulang!',
+    'sent' => 'Kami telah mengirim ulang tautan pengaturan ulang kata sandi Anda!',
+    'token' => 'token reset kata sandi ini tidak valid.',
+    'user' => "Kami tidak dapat menemukan pengguna dengan alamat e-mail itu.",
 
 ];
